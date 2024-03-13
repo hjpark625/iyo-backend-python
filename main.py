46afa5664899b380926388f410395a95390ba93c
hjpark625 <zero950@gmail.com>
2024-03-13 21:36:28 +0900

    feat: CORS 미들웨어 추가

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from detail.app import router as detail_router
 from pins.app import router as pins_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+# origin = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=origin,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(detail_router)
 app.include_router(pins_router)
