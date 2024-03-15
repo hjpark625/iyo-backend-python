f80ac0e271aa9d70a447314466d5a3fb47fc60d5
hjpark625 <zero950@gmail.com>
2024-03-15 17:32:08 +0900

    refactor: status code httpstatus enum 적용

--- detail/app.py
+++ detail/app.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from detail.detail_dto import DetailResponseDTO, DetailDTO
 from database.database import Database
+from enums.http_status import HttpStatus
 
 router = APIRouter()
 
@@ -9,7 +10,8 @@ router = APIRouter()
 def get_detail(storename: str):
     if not storename or storename.strip() == "":
         raise HTTPException(
-            status_code=404, detail={"message": "상점 이름 정보가 없습니다."}
+            status_code=HttpStatus.NOT_FOUND.value,
+            detail={"message": "상점 이름 정보가 없습니다."},
         )
     client = Database(db_name="iyo", table_name="details")
     db = client.get_collection()
@@ -18,7 +20,8 @@ def get_detail(storename: str):
 
     if not detail_data:
         raise HTTPException(
-            status_code=404, detail={"message": "상점 정보가 없습니다."}
+            status_code=HttpStatus.NOT_FOUND.value,
+            detail={"message": "상점 정보가 없습니다."},
         )
 
     detail_dto = DetailDTO(detail_data)
