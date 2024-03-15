762031b55b5dafb930939790da967399e3d88b53
hjpark625 <zero950@gmail.com>
2024-03-15 09:50:38 +0900

    refactor: DB에 상점 정보 없을때 에러핸들링 추가

--- detail/app.py
+++ detail/app.py
@@ -9,12 +9,17 @@ router = APIRouter()
 def get_detail(storename: str):
     if not storename or storename.strip() == "":
         raise HTTPException(
-            status_code=404, detail={"message": "storename is required"}
+            status_code=404, detail={"message": "상점 이름 정보가 없습니다."}
         )
     client = Database(db_name="iyo", table_name="details")
     db = client.get_collection()
 
     detail_data = db.find_one({"engName": storename})
+
+    if not detail_data:
+        raise HTTPException(
+            status_code=404, detail={"message": "상점 정보가 없습니다."}
+        )
 
     detail_dto = DetailDTO(detail_data)
     response_dto = DetailResponseDTO(detail_dto)
