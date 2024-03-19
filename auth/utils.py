922d2372320180c04828aa391b33ed2995e1e4f2
hjpark625 <zero950@gmail.com>
2024-03-19 10:08:50 +0900

    feat: accesstoken 발행 함수 작성

--- auth/utils.py
+++ auth/utils.py
@@ -15,3 +15,23 @@ def check_password(password: str, hashed: str) -> bool:
     is_same = checkpw(password=encoded_password, hashed_password=encoded_hashed)
 
     return is_same
+
+
+def generate_access_token(access_token_payload: AccessTokenPayload):
+    origin_info = access_token_payload.__dict__
+
+    exp_time = datetime.now() + timedelta(minutes=30)
+
+    access_token_payload = {
+        "userId": origin_info["userId"],
+        "email": origin_info["email"],
+        "exp": exp_time,
+    }
+
+    access_token = encode(
+        payload=access_token_payload, key=os.getenv("JWT_SECRET"), algorithm="HS256"
+    )
+
+    print(access_token)
+
+    return access_token
